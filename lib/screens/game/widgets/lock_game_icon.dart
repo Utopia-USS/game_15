cdf58eca3520c39a39512525be22aad21441e179
j8000 <kuba.kirchner@gmail.com>
2022-03-12 12:29:10 +0100

    gamexgame mouse region

--- lib/screens/game/widgets/lock_game_icon.dart
+++ lib/screens/game/widgets/lock_game_icon.dart
@@ -13,27 +13,30 @@ class LockGameIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 32.0),
-      child: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () => onLockChanged(!isLocked),
-        child:   AnimatedSwitcher(
-          duration: const Duration(milliseconds: 400),
-          reverseDuration: const Duration(milliseconds: 400),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(
-                isLocked ? Icons.lock : Icons.lock_open,
-                color: Colors.white,
-              ),
-              const SizedBox(width: 12),
-             Text(
-                  isLocked ? "Unlock game" : "Lock game",
-                  key: ValueKey(isLocked),
-                  style: AppText.menuItem,
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () => onLockChanged(!isLocked),
+          child:   AnimatedSwitcher(
+            duration: const Duration(milliseconds: 400),
+            reverseDuration: const Duration(milliseconds: 400),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  isLocked ? Icons.lock : Icons.lock_open,
+                  color: Colors.white,
                 ),
+                const SizedBox(width: 12),
+               Text(
+                    isLocked ? "Unlock game" : "Lock game",
+                    key: ValueKey(isLocked),
+                    style: AppText.menuItem,
+                  ),
 
-            ],
+              ],
+            ),
           ),
         ),
       ),
